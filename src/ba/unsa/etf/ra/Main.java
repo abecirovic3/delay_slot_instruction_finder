71fa9f9ae70e9a66940d5a02d82113d85b1780b2
Ajdin Bečirović <abecirovic3@etf.unsa.ba>
2020-04-19 20:24:02 +0200

    uvodne poruke

--- src/ba/unsa/etf/ra/Main.java
+++ src/ba/unsa/etf/ra/Main.java
@@ -1,8 +1,19 @@
 package ba.unsa.etf.ra;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Pozdrav\nPrije nego što navedete putanju do datoteke koja sadrži sekvencu instrukcija\n" +
+                "važna napomena: Format instrukcija mora biti takav da je između svake riječi barem jedan razmak, te " +
+                "da su operandi pored razmaka razdvojeni i zarezima!" +
+                "Takođe je bitno da su instrukcije u MIPS formatu npr. add $s0, $s1, $s2");
+
+        System.out.println("Unesite putanju do datoteke sa sekvencom instrukcija:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String path = scanner.nextLine();
     }
 }
